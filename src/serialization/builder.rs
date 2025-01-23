9639e03d984c9897a2d4284a8485b23f190967b9
akarahdev <endisticbutitsforgit@gmail.com>
2025-01-22 23:04:03 -0500

    feat: make record codecs macro-driven

--- src/serialization/builder.rs
+++ src/serialization/builder.rs
@@ -4,7 +4,10 @@ use alloc::string::String;
 
 use super::{
     Codec,
-    record::{RecordCodec1, RecordCodec2, RecordField, UnitCodec},
+    record::{
+        RecordCodec1, RecordCodec2, RecordCodec3, RecordCodec4, RecordCodec5, RecordCodec6,
+        RecordField, UnitCodec,
+    },
 };
 
 pub struct RecordCodecBuilder<InnerCodec> {
@@ -38,40 +41,88 @@ impl RecordCodecBuilder<UnitCodec> {
     }
 }
 
-impl<P1, P1C: Codec<P1>, Struct> RecordCodecBuilder<RecordCodec1<P1, P1C, Struct>> {
-    pub fn field<P2, P2C: Codec<P2>>(
-        self,
-        codec: P2C,
-        field_name: impl Into<String>,
-        getter: fn(&Struct) -> &P2,
-    ) -> RecordCodecBuilder<RecordCodec2<P1, P1C, P2, P2C, Struct>> {
-        RecordCodecBuilder {
-            codec: RecordCodec2 {
-                codec1: self.codec.codec1,
-                codec2: RecordField {
-                    field_name: field_name.into(),
-                    getter,
-                    codec,
-                    _phantom: PhantomData,
-                },
-                into_struct: OnceCell::new(),
-            },
-        }
-    }
+macro_rules! impl_record_codec_builder {
+    (
+        type: $type:ident,
+        fields: { $($field:ident: $name:ident[$codec:ident]),* },
+        next: $next_field_name:ident: $next_type:ident as $next_name:ident[$next_codec:ident]
+    ) => {
+        impl<$($name, $codec: Codec<$name>),*, Struct> RecordCodecBuilder<$type<$($name, $codec),*, Struct>> {
+            pub fn field<$next_name, $next_codec: Codec<$next_name>>(
+                self,
+                codec: $next_codec,
+                field_name: impl Into<String>,
+                getter: fn(&Struct) -> &$next_name,
+            ) -> RecordCodecBuilder<$next_type<$($name, $codec),*, $next_name, $next_codec, Struct>> {
+                RecordCodecBuilder {
+                    codec: $next_type {
+                        $($field: self.codec.$field),*,
+                        $next_field_name: RecordField {
+                            field_name: field_name.into(),
+                            getter,
+                            codec,
+                            _phantom: PhantomData,
+                        },
+                        into_struct: OnceCell::new(),
+                    },
+                }
+            }
 
-    pub fn build(self, into_struct: fn(P1) -> Struct) -> impl Codec<Struct> {
-        self.codec.into_struct.set(into_struct).unwrap();
-        self.codec
-    }
+            pub fn build(self, into_struct: fn($($name),*) -> Struct) -> impl Codec<Struct> {
+                self.codec.into_struct.set(into_struct).unwrap();
+                self.codec
+            }
+        }
+    };
 }
 
-impl<P1, P1C: Codec<P1>, P2, P2C: Codec<P2>, Struct>
-    RecordCodecBuilder<RecordCodec2<P1, P1C, P2, P2C, Struct>>
-{
-    pub fn build(self, into_struct: fn(P1, P2) -> Struct) -> impl Codec<Struct> {
-        self.codec.into_struct.set(into_struct).unwrap();
-        self.codec
-    }
+macro_rules! impl_record_codec_builder_last {
+    (
+        type: $type:ident,
+        fields: { $($field:ident: $name:ident[$codec:ident]),* },
+    ) => {
+        impl<$($name, $codec: Codec<$name>),*, Struct> RecordCodecBuilder<$type<$($name, $codec),*, Struct>> {
+            pub fn build(self, into_struct: fn($($name),*) -> Struct) -> impl Codec<Struct> {
+                self.codec.into_struct.set(into_struct).unwrap();
+                self.codec
+            }
+        }
+    };
+}
+
+impl_record_codec_builder! {
+    type: RecordCodec1,
+    fields: { codec1: P1[P1C] },
+    next: codec2: RecordCodec2 as P2[P2C]
+}
+
+impl_record_codec_builder! {
+    type: RecordCodec2,
+    fields: { codec1: P1[P1C], codec2: P2[P2C] },
+    next: codec3: RecordCodec3 as P3[P3C]
+}
+
+impl_record_codec_builder! {
+    type: RecordCodec3,
+    fields: { codec1: P1[P1C], codec2: P2[P2C], codec3: P3[P3C] },
+    next: codec4: RecordCodec4 as P4[P4C]
+}
+
+impl_record_codec_builder! {
+    type: RecordCodec4,
+    fields: { codec1: P1[P1C], codec2: P2[P2C], codec3: P3[P3C], codec4: P4[P4C] },
+    next: codec5: RecordCodec5 as P5[P5C]
+}
+
+impl_record_codec_builder! {
+    type: RecordCodec5,
+    fields: { codec1: P1[P1C], codec2: P2[P2C], codec3: P3[P3C], codec4: P4[P4C], codec5: P5[P5C] },
+    next: codec6: RecordCodec6 as P6[P6C]
+}
+
+impl_record_codec_builder_last! {
+    type: RecordCodec6,
+    fields: { codec1: P1[P1C], codec2: P2[P2C], codec3: P3[P3C], codec4: P4[P4C], codec5: P5[P5C], codec6: P6[P6C] },
 }
 
 #[cfg(test)]
@@ -82,15 +133,18 @@ mod tests {
     };
 
     #[derive(Debug, PartialEq)]
-    struct Pos2d {
+    struct Pos4d {
         x: f64,
         y: f64,
+        z: f64,
+        w: f64,
     }
 
-    impl Pos2d {
-        fn new(x: f64, y: f64) -> Pos2d {
-            Pos2d { x, y }
+    impl Pos4d {
+        fn new(x: f64, y: f64, z: f64, w: f64) -> Pos4d {
+            Pos4d { x, y, z, w }
         }
+
         fn x(&self) -> &f64 {
             &self.x
         }
@@ -99,20 +153,83 @@ mod tests {
             &self.y
         }
 
-        fn codec() -> impl Codec<Pos2d> {
+        fn z(&self) -> &f64 {
+            &self.z
+        }
+
+        fn w(&self) -> &f64 {
+            &self.w
+        }
+
+        fn codec() -> impl Codec<Pos4d> {
             RecordCodecBuilder::new()
-                .field(f64::codec(), "x", Pos2d::x)
-                .field(f64::codec(), "y", Pos2d::y)
-                .build(Pos2d::new)
+                .field(f64::codec(), "x", Pos4d::x)
+                .field(f64::codec(), "y", Pos4d::y)
+                .field(f64::codec(), "z", Pos4d::z)
+                .field(f64::codec(), "w", Pos4d::w)
+                .build(Pos4d::new)
         }
     }
 
     #[test]
     pub fn simple_record() {
-        let value = Pos2d { x: 10.0, y: 15.0 };
-        let encoded = Pos2d::codec().encode(&Dynamic::ops(), &value).unwrap();
-        let decoded = Pos2d::codec().decode(&Dynamic::ops(), &encoded).unwrap();
+        let value = Pos4d {
+            x: 10.0,
+            y: 15.0,
+            z: 20.0,
+            w: 30.0,
+        };
+        let encoded = Pos4d::codec().encode(&Dynamic::ops(), &value).unwrap();
+        let decoded = Pos4d::codec().decode(&Dynamic::ops(), &encoded).unwrap();
 
         assert_eq!(decoded, value);
+    }
+
+    #[derive(PartialEq, Debug)]
+    struct TopLevel {
+        nested: Nested,
+    }
+
+    impl TopLevel {
+        pub fn nested(&self) -> &Nested {
+            &self.nested
+        }
+        pub fn new(nested: Nested) -> TopLevel {
+            TopLevel { nested }
+        }
+        pub fn codec() -> impl Codec<TopLevel> {
+            RecordCodecBuilder::new()
+                .field(Nested::codec(), "nested", TopLevel::nested)
+                .build(TopLevel::new)
+        }
+    }
+
+    #[derive(PartialEq, Debug)]
+    struct Nested {
+        value: f64,
+    }
+
+    impl Nested {
+        pub fn value(&self) -> &f64 {
+            &self.value
+        }
+        pub fn new(value: f64) -> Nested {
+            Nested { value }
+        }
+        pub fn codec() -> impl Codec<Nested> {
+            RecordCodecBuilder::new()
+                .field(f64::codec(), "value", Nested::value)
+                .build(Nested::new)
+        }
+    }
+
+    #[test]
+    pub fn nested_structs() {
+        let value = TopLevel {
+            nested: Nested { value: 10.0 },
+        };
+        let encoded = TopLevel::codec().encode(&Dynamic::ops(), &value).unwrap();
+        let decoded = TopLevel::codec().decode(&Dynamic::ops(), &encoded).unwrap();
+        assert_eq!(value, decoded);
     }
 }
--- src/serialization/record.rs
+++ src/serialization/record.rs
@@ -25,75 +25,168 @@ impl Codec<()> for UnitCodec {
     }
 }
 
-pub struct RecordCodec1<P1, P1C, Struct>
-where
-    P1C: Codec<P1>,
-{
-    pub(crate) codec1: RecordField<P1, P1C, Struct>,
-    pub(crate) into_struct: OnceCell<fn(P1) -> Struct>,
+macro_rules! record_codec {
+    (
+        name: $struct_name:ident,
+        fields: { $($field:ident: $name:ident[$codec:ident]),* }
+    ) => {
+        pub struct $struct_name<$($name, $codec: Codec<$name>),*, Struct> {
+            $(pub(crate) $field: RecordField<$name, $codec, Struct>),*,
+            pub(crate) into_struct: OnceCell<fn($($name),*) -> Struct>
+        }
+
+        impl<$($name, $codec: Codec<$name>),*, Struct> Codec<Struct> for $struct_name<$($name, $codec),*, Struct> {
+            fn encode<U, O: CodecOps<U>>(&self, ops: &O, value: &Struct) -> DataResult<U> {
+                Ok(ops.create_object(&[
+                    $((
+                        &self.$field.field_name,
+                        self.$field.codec.encode(ops, (self.$field.getter)(value))?,
+                    )),*
+                ]))
+            }
+
+            fn decode<U, O: CodecOps<U>>(&self, ops: &O, value: &U) -> DataResult<Struct> {
+                let obj = ops.get_object(value)?;
+                $(
+                    let Some($field) = obj.get(&self.$field.field_name) else {
+                        return Err(DataError::new(""));
+                    };
+                )*
+
+                Ok((self.into_struct.get().unwrap())(
+                    $((self.$field.codec.decode(ops, $field))?),*
+                ))
+            }
+        }
+    };
 }
 
-impl<P1, P1C, Struct> Codec<Struct> for RecordCodec1<P1, P1C, Struct>
-where
-    P1C: Codec<P1>,
-{
-    fn encode<U, O: CodecOps<U>>(&self, ops: &O, value: &Struct) -> DataResult<U> {
-        Ok(ops.create_object(&[(
-            &self.codec1.field_name,
-            self.codec1.codec.encode(ops, (self.codec1.getter)(value))?,
-        )]))
-    }
-
-    fn decode<U, O: CodecOps<U>>(&self, ops: &O, value: &U) -> DataResult<Struct> {
-        let obj = ops.get_object(value)?;
-        let Some(p1) = obj.get(&self.codec1.field_name) else {
-            return Err(DataError::new(""));
-        };
-        Ok((self.into_struct.get().unwrap())(
-            self.codec1.codec.decode(ops, p1)?,
-        ))
+record_codec! {
+    name: RecordCodec1,
+    fields: {
+        codec1: P1[P1C]
     }
 }
 
-pub struct RecordCodec2<P1, P1C, P2, P2C, Struct>
-where
-    P1C: Codec<P1>,
-    P2C: Codec<P2>,
-{
-    pub(crate) codec1: RecordField<P1, P1C, Struct>,
-    pub(crate) codec2: RecordField<P2, P2C, Struct>,
-    pub(crate) into_struct: OnceCell<fn(P1, P2) -> Struct>,
-}
-
-impl<P1, P1C, P2, P2C, Struct> Codec<Struct> for RecordCodec2<P1, P1C, P2, P2C, Struct>
-where
-    P1C: Codec<P1>,
-    P2C: Codec<P2>,
-{
-    fn encode<U, O: CodecOps<U>>(&self, ops: &O, value: &Struct) -> DataResult<U> {
-        Ok(ops.create_object(&[
-            (
-                &self.codec1.field_name,
-                self.codec1.codec.encode(ops, (self.codec1.getter)(value))?,
-            ),
-            (
-                &self.codec2.field_name,
-                self.codec2.codec.encode(ops, (self.codec2.getter)(value))?,
-            ),
-        ]))
-    }
-
-    fn decode<U, O: CodecOps<U>>(&self, ops: &O, value: &U) -> DataResult<Struct> {
-        let obj = ops.get_object(value)?;
-        let Some(p1) = obj.get(&self.codec1.field_name) else {
-            return Err(DataError::new(""));
-        };
-        let Some(p2) = obj.get(&self.codec2.field_name) else {
-            return Err(DataError::new(""));
-        };
-        Ok((self.into_struct.get().unwrap())(
-            self.codec1.codec.decode(ops, p1)?,
-            self.codec2.codec.decode(ops, p2)?,
-        ))
+record_codec! {
+    name: RecordCodec2,
+    fields: {
+        codec1: P1[P1C],
+        codec2: P2[P2C]
     }
 }
+
+record_codec! {
+    name: RecordCodec3,
+    fields: {
+        codec1: P1[P1C],
+        codec2: P2[P2C],
+        codec3: P3[P3C]
+    }
+}
+
+record_codec! {
+    name: RecordCodec4,
+    fields: {
+        codec1: P1[P1C],
+        codec2: P2[P2C],
+        codec3: P3[P3C],
+        codec4: P4[P4C]
+    }
+}
+
+record_codec! {
+    name: RecordCodec5,
+    fields: {
+        codec1: P1[P1C],
+        codec2: P2[P2C],
+        codec3: P3[P3C],
+        codec4: P4[P4C],
+        codec5: P5[P5C]
+    }
+}
+
+record_codec! {
+    name: RecordCodec6,
+    fields: {
+        codec1: P1[P1C],
+        codec2: P2[P2C],
+        codec3: P3[P3C],
+        codec4: P4[P4C],
+        codec5: P5[P5C],
+        codec6: P6[P6C]
+    }
+}
+
+// pub struct RecordCodec1<P1, P1C, Struct>
+// where
+//     P1C: Codec<P1>,
+// {
+//     pub(crate) codec1: RecordField<P1, P1C, Struct>,
+//     pub(crate) into_struct: OnceCell<fn(P1) -> Struct>,
+// }
+
+// impl<P1, P1C, Struct> Codec<Struct> for RecordCodec1<P1, P1C, Struct>
+// where
+//     P1C: Codec<P1>,
+// {
+//     fn encode<U, O: CodecOps<U>>(&self, ops: &O, value: &Struct) -> DataResult<U> {
+//         Ok(ops.create_object(&[(
+//             &self.codec1.field_name,
+//             self.codec1.codec.encode(ops, (self.codec1.getter)(value))?,
+//         )]))
+//     }
+
+//     fn decode<U, O: CodecOps<U>>(&self, ops: &O, value: &U) -> DataResult<Struct> {
+//         let obj = ops.get_object(value)?;
+//         let Some(p1) = obj.get(&self.codec1.field_name) else {
+//             return Err(DataError::new(""));
+//         };
+//         Ok((self.into_struct.get().unwrap())(
+//             self.codec1.codec.decode(ops, p1)?,
+//         ))
+//     }
+// }
+
+// pub struct RecordCodec2<P1, P1C, P2, P2C, Struct>
+// where
+//     P1C: Codec<P1>,
+//     P2C: Codec<P2>,
+// {
+//     pub(crate) codec1: RecordField<P1, P1C, Struct>,
+//     pub(crate) codec2: RecordField<P2, P2C, Struct>,
+//     pub(crate) into_struct: OnceCell<fn(P1, P2) -> Struct>,
+// }
+
+// impl<P1, P1C, P2, P2C, Struct> Codec<Struct> for RecordCodec2<P1, P1C, P2, P2C, Struct>
+// where
+//     P1C: Codec<P1>,
+//     P2C: Codec<P2>,
+// {
+//     fn encode<U, O: CodecOps<U>>(&self, ops: &O, value: &Struct) -> DataResult<U> {
+//         Ok(ops.create_object(&[
+//             (
+//                 &self.codec1.field_name,
+//                 self.codec1.codec.encode(ops, (self.codec1.getter)(value))?,
+//             ),
+//             (
+//                 &self.codec2.field_name,
+//                 self.codec2.codec.encode(ops, (self.codec2.getter)(value))?,
+//             ),
+//         ]))
+//     }
+
+//     fn decode<U, O: CodecOps<U>>(&self, ops: &O, value: &U) -> DataResult<Struct> {
+//         let obj = ops.get_object(value)?;
+//         let Some(p1) = obj.get(&self.codec1.field_name) else {
+//             return Err(DataError::new(""));
+//         };
+//         let Some(p2) = obj.get(&self.codec2.field_name) else {
+//             return Err(DataError::new(""));
+//         };
+//         Ok((self.into_struct.get().unwrap())(
+//             self.codec1.codec.decode(ops, p1)?,
+//             self.codec2.codec.decode(ops, p2)?,
+//         ))
+//     }
+// }
