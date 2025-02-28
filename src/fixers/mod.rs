89dbddbe87a572e5525ff140bdf0de78879706c2
akarahdev <endisticbutitsforgit@gmail.com>
2025-02-27 19:59:17 -0500

    feat: remove TyVal api, will replace with Dynamic and Type apis, both seperated

--- src/fixers/mod.rs
+++ src/fixers/mod.rs
@@ -1,9 +1,7 @@
-mod types;
 use core::marker::PhantomData;
 
+mod types;
 pub use types::*;
-mod tv;
-pub use tv::*;
 mod rules;
 pub use rules::*;
 
@@ -12,13 +10,6 @@ use crate::serialization::CodecOps;
 pub trait TypeRewriteRule<OT, O: CodecOps<OT>> {
     fn fix_data(&self, ops: O, value: OT) -> OT;
     fn fix_type(&self, ty: Type) -> Type;
-
-    fn fix_tyval(&self, ty_val: TyVal<OT, O>) -> TyVal<OT, O> {
-        match ty_val {
-            TyVal::Value(value, ops) => TyVal::Value(self.fix_data(ops.clone(), value), ops),
-            TyVal::Type(ty) => TyVal::Type(self.fix_type(ty)),
-        }
-    }
 
     fn and_then(self, other: impl TypeRewriteRule<OT, O>) -> impl TypeRewriteRule<OT, O>
     where
--- src/fixers/rules.rs
+++ src/fixers/rules.rs
@@ -4,20 +4,11 @@ use alloc::string::{String, ToString};
 
 use crate::serialization::{CodecOps, MapViewMut, OwnedMapView};
 
-use super::{TyVal, Type, TypeRewriteRule};
+use super::{Type, TypeRewriteRule};
 
 pub struct Rules;
 
 impl Rules {
-    pub fn map<OT, O: CodecOps<OT>, F: Fn(TyVal<OT, O>) -> TyVal<OT, O>>(
-        function: F,
-    ) -> impl TypeRewriteRule<OT, O> {
-        MapFieldRule {
-            function,
-            _phantom: PhantomData,
-        }
-    }
-
     pub fn new_field<OT, O: CodecOps<OT>, F: Fn(&OT) -> OT, G: Fn(&Type) -> Type>(
         field_name: &str,
         value_function: F,
@@ -46,31 +37,6 @@ impl Rules {
             field_name: field_name.to_string(),
             rule,
             _phantom: PhantomData,
-        }
-    }
-}
-
-pub struct MapFieldRule<OT, O: CodecOps<OT>, F: Fn(TyVal<OT, O>) -> TyVal<OT, O>> {
-    function: F,
-    _phantom: PhantomData<(OT, O)>,
-}
-
-impl<OT, O: CodecOps<OT>, F: Fn(TyVal<OT, O>) -> TyVal<OT, O>> TypeRewriteRule<OT, O>
-    for MapFieldRule<OT, O, F>
-{
-    fn fix_data(&self, ops: O, value: OT) -> OT {
-        let result = (self.function)(TyVal::Value(value, ops));
-        match result {
-            TyVal::Value(value, _) => value,
-            TyVal::Type(_) => panic!(),
-        }
-    }
-
-    fn fix_type(&self, ty: Type) -> Type {
-        let result = (self.function)(TyVal::Type(ty));
-        match result {
-            TyVal::Value(_, _) => panic!(),
-            TyVal::Type(ty) => ty,
         }
     }
 }
